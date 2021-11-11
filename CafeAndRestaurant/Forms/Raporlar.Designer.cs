00c3af19f4ecf5487568473a6b04260e6107e589
fatmaunlu <fatmaunluu93@gmail.com>
2021-11-12 00:42:54 +0300

    Raporlar formu oluşturuldu

---
+++ CafeAndRestaurant/Forms/Raporlar.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace CafeAndRestaurant.Forms
+{
+    partial class Raporlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpBoxGunlukRapor = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstBoxFiyatGunluk = new System.Windows.Forms.ListBox();
+            this.lstBoxUrununMiktariGunluk = new System.Windows.Forms.ListBox();
+            this.lstBoxUrununAdiGunluk = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpBoxAylikRapor = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lstBoxFiyatAylik = new System.Windows.Forms.ListBox();
+            this.lstBoxUrununMiktariAylik = new System.Windows.Forms.ListBox();
+            this.lstBoxUrununAdiAylik = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.grpBoxGunlukRapor.SuspendLayout();
+            this.grpBoxAylikRapor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBoxGunlukRapor
+            // 
+            this.grpBoxGunlukRapor.Controls.Add(this.button1);
+            this.grpBoxGunlukRapor.Controls.Add(this.label5);
+            this.grpBoxGunlukRapor.Controls.Add(this.label4);
+            this.grpBoxGunlukRapor.Controls.Add(this.label3);
+            this.grpBoxGunlukRapor.Controls.Add(this.lstBoxFiyatGunluk);
+            this.grpBoxGunlukRapor.Controls.Add(this.lstBoxUrununMiktariGunluk);
+            this.grpBoxGunlukRapor.Controls.Add(this.lstBoxUrununAdiGunluk);
+            this.grpBoxGunlukRapor.Controls.Add(this.label1);
+            this.grpBoxGunlukRapor.Location = new System.Drawing.Point(12, 3);
+            this.grpBoxGunlukRapor.Name = "grpBoxGunlukRapor";
+            this.grpBoxGunlukRapor.Size = new System.Drawing.Size(473, 522);
+            this.grpBoxGunlukRapor.TabIndex = 0;
+            this.grpBoxGunlukRapor.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(375, 476);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Rapor Al";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(375, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Fiyat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(196, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ürünün Miktarı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ürünün Adı";
+            // 
+            // lstBoxFiyatGunluk
+            // 
+            this.lstBoxFiyatGunluk.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBoxFiyatGunluk.ForeColor = System.Drawing.Color.White;
+            this.lstBoxFiyatGunluk.FormattingEnabled = true;
+            this.lstBoxFiyatGunluk.ItemHeight = 15;
+            this.lstBoxFiyatGunluk.Location = new System.Drawing.Point(317, 66);
+            this.lstBoxFiyatGunluk.Name = "lstBoxFiyatGunluk";
+            this.lstBoxFiyatGunluk.Size = new System.Drawing.Size(149, 195);
+            this.lstBoxFiyatGunluk.TabIndex = 6;
+            // 
+            // lstBoxUrununMiktariGunluk
+            // 
+            this.lstBoxUrununMiktariGunluk.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBoxUrununMiktariGunluk.ForeColor = System.Drawing.Color.White;
+            this.lstBoxUrununMiktariGunluk.FormattingEnabled = true;
+            this.lstBoxUrununMiktariGunluk.ItemHeight = 15;
+            this.lstBoxUrununMiktariGunluk.Location = new System.Drawing.Point(162, 66);
+            this.lstBoxUrununMiktariGunluk.Name = "lstBoxUrununMiktariGunluk";
+            this.lstBoxUrununMiktariGunluk.Size = new System.Drawing.Size(149, 195);
+            this.lstBoxUrununMiktariGunluk.TabIndex = 5;
+            // 
+            // lstBoxUrununAdiGunluk
+            // 
+            this.lstBoxUrununAdiGunluk.BackColor = System.Drawing.Color.White;
+            this.lstBoxUrununAdiGunluk.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBoxUrununAdiGunluk.ForeColor = System.Drawing.Color.White;
+            this.lstBoxUrununAdiGunluk.FormattingEnabled = true;
+            this.lstBoxUrununAdiGunluk.HorizontalScrollbar = true;
+            this.lstBoxUrununAdiGunluk.ItemHeight = 15;
+            this.lstBoxUrununAdiGunluk.Location = new System.Drawing.Point(7, 66);
+            this.lstBoxUrununAdiGunluk.Name = "lstBoxUrununAdiGunluk";
+            this.lstBoxUrununAdiGunluk.Size = new System.Drawing.Size(149, 195);
+            this.lstBoxUrununAdiGunluk.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Günlük Rapor                                                                     " +
+    "                                                          ";
+            // 
+            // grpBoxAylikRapor
+            // 
+            this.grpBoxAylikRapor.Controls.Add(this.button2);
+            this.grpBoxAylikRapor.Controls.Add(this.lstBoxFiyatAylik);
+            this.grpBoxAylikRapor.Controls.Add(this.lstBoxUrununMiktariAylik);
+            this.grpBoxAylikRapor.Controls.Add(this.lstBoxUrununAdiAylik);
+            this.grpBoxAylikRapor.Controls.Add(this.label2);
+            this.grpBoxAylikRapor.Controls.Add(this.label6);
+            this.grpBoxAylikRapor.Controls.Add(this.label7);
+            this.grpBoxAylikRapor.Controls.Add(this.label8);
+            this.grpBoxAylikRapor.Location = new System.Drawing.Point(491, 3);
+            this.grpBoxAylikRapor.Name = "grpBoxAylikRapor";
+            this.grpBoxAylikRapor.Size = new System.Drawing.Size(473, 522);
+            this.grpBoxAylikRapor.TabIndex = 10;
+            this.grpBoxAylikRapor.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Location = new System.Drawing.Point(374, 476);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 31);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Rapor Al";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // lstBoxFiyatAylik
+            // 
+            this.lstBoxFiyatAylik.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBoxFiyatAylik.ForeColor = System.Drawing.Color.White;
+            this.lstBoxFiyatAylik.FormattingEnabled = true;
+            this.lstBoxFiyatAylik.ItemHeight = 15;
+            this.lstBoxFiyatAylik.Location = new System.Drawing.Point(316, 66);
+            this.lstBoxFiyatAylik.Name = "lstBoxFiyatAylik";
+            this.lstBoxFiyatAylik.Size = new System.Drawing.Size(149, 195);
+            this.lstBoxFiyatAylik.TabIndex = 12;
+            // 
+            // lstBoxUrununMiktariAylik
+            // 
+            this.lstBoxUrununMiktariAylik.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBoxUrununMiktariAylik.ForeColor = System.Drawing.Color.White;
+            this.lstBoxUrununMiktariAylik.FormattingEnabled = true;
+            this.lstBoxUrununMiktariAylik.ItemHeight = 15;
+            this.lstBoxUrununMiktariAylik.Location = new System.Drawing.Point(161, 66);
+            this.lstBoxUrununMiktariAylik.Name = "lstBoxUrununMiktariAylik";
+            this.lstBoxUrununMiktariAylik.Size = new System.Drawing.Size(149, 195);
+            this.lstBoxUrununMiktariAylik.TabIndex = 11;
+            // 
+            // lstBoxUrununAdiAylik
+            // 
+            this.lstBoxUrununAdiAylik.BackColor = System.Drawing.Color.White;
+            this.lstBoxUrununAdiAylik.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBoxUrununAdiAylik.ForeColor = System.Drawing.Color.White;
+            this.lstBoxUrununAdiAylik.FormattingEnabled = true;
+            this.lstBoxUrununAdiAylik.HorizontalScrollbar = true;
+            this.lstBoxUrununAdiAylik.ItemHeight = 15;
+            this.lstBoxUrununAdiAylik.Location = new System.Drawing.Point(6, 66);
+            this.lstBoxUrununAdiAylik.Name = "lstBoxUrununAdiAylik";
+            this.lstBoxUrununAdiAylik.Size = new System.Drawing.Size(149, 195);
+            this.lstBoxUrununAdiAylik.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Fiyat";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(199, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Ürünün Miktarı";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Ürünün Adı";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(6, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(460, 15);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Günlük Rapor                                                                     " +
+    "                                                          ";
+            // 
+            // Raporlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(975, 546);
+            this.Controls.Add(this.grpBoxAylikRapor);
+            this.Controls.Add(this.grpBoxGunlukRapor);
+            this.Name = "Raporlar";
+            this.Text = "Raporlar";
+            this.grpBoxGunlukRapor.ResumeLayout(false);
+            this.grpBoxGunlukRapor.PerformLayout();
+            this.grpBoxAylikRapor.ResumeLayout(false);
+            this.grpBoxAylikRapor.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox grpBoxGunlukRapor;
+        private ListBox lstBoxFiyatGunluk;
+        private ListBox lstBoxUrununMiktariGunluk;
+        private ListBox lstBoxUrununAdiGunluk;
+        private Label label1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private GroupBox grpBoxAylikRapor;
+        private ListBox lstBoxFiyatAylik;
+        private ListBox lstBoxUrununMiktariAylik;
+        private ListBox lstBoxUrununAdiAylik;
+        private Label label2;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Button button1;
+        private Button button2;
+    }
+}
