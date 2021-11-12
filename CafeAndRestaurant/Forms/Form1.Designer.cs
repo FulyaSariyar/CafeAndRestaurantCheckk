6c308f25dcdac94f03e01a8fb7b9db392400751c
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-11-12 11:51:24 +0300

    #1 Form değişiklikleri son.

--- CafeAndRestaurant/Forms/Form1.Designer.cs
+++ CafeAndRestaurant/Forms/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.cb3 = new System.Windows.Forms.CheckBox();
             this.cb2 = new System.Windows.Forms.CheckBox();
             this.cb1 = new System.Windows.Forms.CheckBox();
+            this.btnNext1 = new System.Windows.Forms.Button();
             this.gbBinaBilgileri.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,13 +61,13 @@
             this.gbBinaBilgileri.Controls.Add(this.cb3);
             this.gbBinaBilgileri.Controls.Add(this.cb2);
             this.gbBinaBilgileri.Controls.Add(this.cb1);
-            this.gbBinaBilgileri.Dock = System.Windows.Forms.DockStyle.Left;
             this.gbBinaBilgileri.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.gbBinaBilgileri.Location = new System.Drawing.Point(0, 0);
+            this.gbBinaBilgileri.ForeColor = System.Drawing.Color.Black;
+            this.gbBinaBilgileri.Location = new System.Drawing.Point(-3, 2);
             this.gbBinaBilgileri.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.gbBinaBilgileri.Name = "gbBinaBilgileri";
             this.gbBinaBilgileri.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.gbBinaBilgileri.Size = new System.Drawing.Size(323, 496);
+            this.gbBinaBilgileri.Size = new System.Drawing.Size(310, 495);
             this.gbBinaBilgileri.TabIndex = 0;
             this.gbBinaBilgileri.TabStop = false;
             this.gbBinaBilgileri.Tag = "";
@@ -74,55 +75,54 @@
             // 
             // comboBox6
             // 
-            this.comboBox6.BackColor = System.Drawing.Color.LightGray;
-            this.comboBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox6.ForeColor = System.Drawing.Color.Silver;
             this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(170, 185);
+            this.comboBox6.Location = new System.Drawing.Point(147, 252);
             this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(121, 28);
+            this.comboBox6.Size = new System.Drawing.Size(121, 30);
             this.comboBox6.TabIndex = 11;
             // 
             // comboBox5
             // 
-            this.comboBox5.BackColor = System.Drawing.Color.LightGray;
+            this.comboBox5.ForeColor = System.Drawing.Color.Silver;
             this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(170, 158);
+            this.comboBox5.Location = new System.Drawing.Point(147, 212);
             this.comboBox5.Name = "comboBox5";
             this.comboBox5.Size = new System.Drawing.Size(121, 30);
             this.comboBox5.TabIndex = 10;
             // 
             // comboBox4
             // 
-            this.comboBox4.BackColor = System.Drawing.Color.LightGray;
+            this.comboBox4.ForeColor = System.Drawing.Color.Silver;
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(170, 128);
+            this.comboBox4.Location = new System.Drawing.Point(147, 176);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(121, 30);
             this.comboBox4.TabIndex = 9;
             // 
             // comboBox3
             // 
-            this.comboBox3.BackColor = System.Drawing.Color.LightGray;
+            this.comboBox3.ForeColor = System.Drawing.Color.Silver;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(170, 99);
+            this.comboBox3.Location = new System.Drawing.Point(147, 140);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 30);
             this.comboBox3.TabIndex = 8;
             // 
             // comboBox2
             // 
-            this.comboBox2.BackColor = System.Drawing.Color.LightGray;
+            this.comboBox2.ForeColor = System.Drawing.Color.Silver;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(170, 72);
+            this.comboBox2.Location = new System.Drawing.Point(147, 101);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 30);
             this.comboBox2.TabIndex = 7;
             // 
             // comboBox1
             // 
-            this.comboBox1.BackColor = System.Drawing.Color.LightGray;
+            this.comboBox1.ForeColor = System.Drawing.Color.Silver;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(170, 42);
+            this.comboBox1.Location = new System.Drawing.Point(147, 62);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 30);
             this.comboBox1.TabIndex = 6;
@@ -131,19 +131,20 @@
             // 
             this.cb6.AutoSize = true;
             this.cb6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cb6.Location = new System.Drawing.Point(22, 185);
+            this.cb6.Location = new System.Drawing.Point(8, 252);
             this.cb6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cb6.Name = "cb6";
             this.cb6.Size = new System.Drawing.Size(61, 24);
             this.cb6.TabIndex = 5;
             this.cb6.Text = "Kat4";
             this.cb6.UseVisualStyleBackColor = true;
+            this.cb6.CheckedChanged += new System.EventHandler(this.cb6_CheckedChanged);
             // 
             // cb5
             // 
             this.cb5.AutoSize = true;
             this.cb5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cb5.Location = new System.Drawing.Point(22, 158);
+            this.cb5.Location = new System.Drawing.Point(8, 212);
             this.cb5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cb5.Name = "cb5";
             this.cb5.Size = new System.Drawing.Size(61, 24);
@@ -155,7 +156,7 @@
             // 
             this.cb4.AutoSize = true;
             this.cb4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cb4.Location = new System.Drawing.Point(22, 131);
+            this.cb4.Location = new System.Drawing.Point(8, 173);
             this.cb4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cb4.Name = "cb4";
             this.cb4.Size = new System.Drawing.Size(61, 24);
@@ -167,7 +168,7 @@
             // 
             this.cb3.AutoSize = true;
             this.cb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cb3.Location = new System.Drawing.Point(22, 105);
+            this.cb3.Location = new System.Drawing.Point(8, 140);
             this.cb3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cb3.Name = "cb3";
             this.cb3.Size = new System.Drawing.Size(65, 24);
@@ -179,7 +180,8 @@
             // 
             this.cb2.AutoSize = true;
             this.cb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cb2.Location = new System.Drawing.Point(22, 78);
+            this.cb2.ForeColor = System.Drawing.Color.Black;
+            this.cb2.Location = new System.Drawing.Point(8, 101);
             this.cb2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cb2.Name = "cb2";
             this.cb2.Size = new System.Drawing.Size(100, 24);
@@ -191,7 +193,7 @@
             // 
             this.cb1.AutoSize = true;
             this.cb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cb1.Location = new System.Drawing.Point(22, 51);
+            this.cb1.Location = new System.Drawing.Point(8, 62);
             this.cb1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cb1.Name = "cb1";
             this.cb1.Size = new System.Drawing.Size(132, 24);
@@ -199,16 +201,32 @@
             this.cb1.Text = "Bahçe Bölümü";
             this.cb1.UseVisualStyleBackColor = true;
             // 
+            // btnNext1
+            // 
+            this.btnNext1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext1.BackColor = System.Drawing.Color.White;
+            this.btnNext1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNext1.ForeColor = System.Drawing.Color.Black;
+            this.btnNext1.Location = new System.Drawing.Point(1123, 454);
+            this.btnNext1.Name = "btnNext1";
+            this.btnNext1.Size = new System.Drawing.Size(114, 30);
+            this.btnNext1.TabIndex = 1;
+            this.btnNext1.Text = "Next";
+            this.btnNext1.UseVisualStyleBackColor = false;
+            this.btnNext1.Click += new System.EventHandler(this.btnNext1_Click_1);
+            // 
             // FrmIlk
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Silver;
+            this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1249, 496);
+            this.Controls.Add(this.btnNext1);
             this.Controls.Add(this.gbBinaBilgileri);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Silver;
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "FrmIlk";
             this.Text = "Hoşgeldiniz";
@@ -228,11 +246,12 @@
         private CheckBox cb3;
         private CheckBox cb2;
         private CheckBox cb1;
-        private ComboBox comboBox6;
         private ComboBox comboBox5;
         private ComboBox comboBox4;
         private ComboBox comboBox3;
         private ComboBox comboBox2;
         private ComboBox comboBox1;
+        private ComboBox comboBox6;
+        private Button btnNext1;
     }
 }
--- CafeAndRestaurant/Forms/Form1.cs
+++ CafeAndRestaurant/Forms/Form1.cs
@@ -25,5 +25,24 @@ namespace CafeAndRestaurant
         {
 
         }
+
+        private void btnNext1_Click(object sender, EventArgs e)
+        {
+            FrmGiris frmGiris = new FrmGiris();
+            frmGiris.Show();  
+            this.Hide();
+        }
+
+        private void cb6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNext1_Click_1(object sender, EventArgs e)
+        {
+            FrmGiris frmGiris = new FrmGiris();
+            frmGiris.Show();
+            this.Hide();
+        }
     }
 }
