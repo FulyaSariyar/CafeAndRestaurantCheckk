f1b06ebee978abf58b21d1baaa05813f581843a7
fatmaunlu <fatmaunluu93@gmail.com>
2021-11-12 09:23:54 +0300

    rapor formu güncellendi 3

--- CafeAndRestaurant/Forms/Raporlar.Designer.cs
+++ CafeAndRestaurant/Forms/Raporlar.Designer.cs
@@ -29,37 +29,32 @@ namespace CafeAndRestaurant.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.grpBoxGunlukRapor = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.lstBoxFiyatGunluk = new System.Windows.Forms.ListBox();
-            this.lstBoxUrununMiktariGunluk = new System.Windows.Forms.ListBox();
-            this.lstBoxUrununAdiGunluk = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.grpBoxAylikRapor = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.lstBoxFiyatAylik = new System.Windows.Forms.ListBox();
-            this.lstBoxUrununMiktariAylik = new System.Windows.Forms.ListBox();
-            this.lstBoxUrununAdiAylik = new System.Windows.Forms.ListBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.dgwAylikRapor = new System.Windows.Forms.DataGridView();
+            this.urnAdiAylik = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urnMiktarAylik = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urnFiyatAylik = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.urunAdiGunluk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urunMiktariGunluk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urnFiyatGunluk = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.grpBoxGunlukRapor.SuspendLayout();
             this.grpBoxAylikRapor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwAylikRapor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // grpBoxGunlukRapor
             // 
+            this.grpBoxGunlukRapor.Controls.Add(this.dataGridView1);
             this.grpBoxGunlukRapor.Controls.Add(this.button1);
-            this.grpBoxGunlukRapor.Controls.Add(this.label5);
-            this.grpBoxGunlukRapor.Controls.Add(this.label4);
-            this.grpBoxGunlukRapor.Controls.Add(this.label3);
-            this.grpBoxGunlukRapor.Controls.Add(this.lstBoxFiyatGunluk);
-            this.grpBoxGunlukRapor.Controls.Add(this.lstBoxUrununMiktariGunluk);
-            this.grpBoxGunlukRapor.Controls.Add(this.lstBoxUrununAdiGunluk);
             this.grpBoxGunlukRapor.Controls.Add(this.label1);
             this.grpBoxGunlukRapor.Location = new System.Drawing.Point(12, 3);
             this.grpBoxGunlukRapor.Name = "grpBoxGunlukRapor";
@@ -77,68 +72,6 @@ namespace CafeAndRestaurant.Forms
             this.button1.Text = "Rapor Al";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(375, 48);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 15);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Fiyat";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(196, 48);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 15);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Ürünün Miktarı";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(47, 45);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 15);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Ürünün Adı";
-            // 
-            // lstBoxFiyatGunluk
-            // 
-            this.lstBoxFiyatGunluk.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lstBoxFiyatGunluk.ForeColor = System.Drawing.Color.White;
-            this.lstBoxFiyatGunluk.FormattingEnabled = true;
-            this.lstBoxFiyatGunluk.ItemHeight = 15;
-            this.lstBoxFiyatGunluk.Location = new System.Drawing.Point(317, 66);
-            this.lstBoxFiyatGunluk.Name = "lstBoxFiyatGunluk";
-            this.lstBoxFiyatGunluk.Size = new System.Drawing.Size(149, 195);
-            this.lstBoxFiyatGunluk.TabIndex = 6;
-            // 
-            // lstBoxUrununMiktariGunluk
-            // 
-            this.lstBoxUrununMiktariGunluk.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lstBoxUrununMiktariGunluk.ForeColor = System.Drawing.Color.White;
-            this.lstBoxUrununMiktariGunluk.FormattingEnabled = true;
-            this.lstBoxUrununMiktariGunluk.ItemHeight = 15;
-            this.lstBoxUrununMiktariGunluk.Location = new System.Drawing.Point(162, 66);
-            this.lstBoxUrununMiktariGunluk.Name = "lstBoxUrununMiktariGunluk";
-            this.lstBoxUrununMiktariGunluk.Size = new System.Drawing.Size(149, 195);
-            this.lstBoxUrununMiktariGunluk.TabIndex = 5;
-            // 
-            // lstBoxUrununAdiGunluk
-            // 
-            this.lstBoxUrununAdiGunluk.BackColor = System.Drawing.Color.White;
-            this.lstBoxUrununAdiGunluk.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lstBoxUrununAdiGunluk.ForeColor = System.Drawing.Color.White;
-            this.lstBoxUrununAdiGunluk.FormattingEnabled = true;
-            this.lstBoxUrununAdiGunluk.HorizontalScrollbar = true;
-            this.lstBoxUrununAdiGunluk.ItemHeight = 15;
-            this.lstBoxUrununAdiGunluk.Location = new System.Drawing.Point(7, 66);
-            this.lstBoxUrununAdiGunluk.Name = "lstBoxUrununAdiGunluk";
-            this.lstBoxUrununAdiGunluk.Size = new System.Drawing.Size(149, 195);
-            this.lstBoxUrununAdiGunluk.TabIndex = 4;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -152,13 +85,8 @@ namespace CafeAndRestaurant.Forms
             // 
             // grpBoxAylikRapor
             // 
+            this.grpBoxAylikRapor.Controls.Add(this.dgwAylikRapor);
             this.grpBoxAylikRapor.Controls.Add(this.button2);
-            this.grpBoxAylikRapor.Controls.Add(this.lstBoxFiyatAylik);
-            this.grpBoxAylikRapor.Controls.Add(this.lstBoxUrununMiktariAylik);
-            this.grpBoxAylikRapor.Controls.Add(this.lstBoxUrununAdiAylik);
-            this.grpBoxAylikRapor.Controls.Add(this.label2);
-            this.grpBoxAylikRapor.Controls.Add(this.label6);
-            this.grpBoxAylikRapor.Controls.Add(this.label7);
             this.grpBoxAylikRapor.Controls.Add(this.label8);
             this.grpBoxAylikRapor.Location = new System.Drawing.Point(491, 3);
             this.grpBoxAylikRapor.Name = "grpBoxAylikRapor";
@@ -176,68 +104,6 @@ namespace CafeAndRestaurant.Forms
             this.button2.Text = "Rapor Al";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // lstBoxFiyatAylik
-            // 
-            this.lstBoxFiyatAylik.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lstBoxFiyatAylik.ForeColor = System.Drawing.Color.White;
-            this.lstBoxFiyatAylik.FormattingEnabled = true;
-            this.lstBoxFiyatAylik.ItemHeight = 15;
-            this.lstBoxFiyatAylik.Location = new System.Drawing.Point(316, 66);
-            this.lstBoxFiyatAylik.Name = "lstBoxFiyatAylik";
-            this.lstBoxFiyatAylik.Size = new System.Drawing.Size(149, 195);
-            this.lstBoxFiyatAylik.TabIndex = 12;
-            // 
-            // lstBoxUrununMiktariAylik
-            // 
-            this.lstBoxUrununMiktariAylik.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lstBoxUrununMiktariAylik.ForeColor = System.Drawing.Color.White;
-            this.lstBoxUrununMiktariAylik.FormattingEnabled = true;
-            this.lstBoxUrununMiktariAylik.ItemHeight = 15;
-            this.lstBoxUrununMiktariAylik.Location = new System.Drawing.Point(161, 66);
-            this.lstBoxUrununMiktariAylik.Name = "lstBoxUrununMiktariAylik";
-            this.lstBoxUrununMiktariAylik.Size = new System.Drawing.Size(149, 195);
-            this.lstBoxUrununMiktariAylik.TabIndex = 11;
-            // 
-            // lstBoxUrununAdiAylik
-            // 
-            this.lstBoxUrununAdiAylik.BackColor = System.Drawing.Color.White;
-            this.lstBoxUrununAdiAylik.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lstBoxUrununAdiAylik.ForeColor = System.Drawing.Color.White;
-            this.lstBoxUrununAdiAylik.FormattingEnabled = true;
-            this.lstBoxUrununAdiAylik.HorizontalScrollbar = true;
-            this.lstBoxUrununAdiAylik.ItemHeight = 15;
-            this.lstBoxUrununAdiAylik.Location = new System.Drawing.Point(6, 66);
-            this.lstBoxUrununAdiAylik.Name = "lstBoxUrununAdiAylik";
-            this.lstBoxUrununAdiAylik.Size = new System.Drawing.Size(149, 195);
-            this.lstBoxUrununAdiAylik.TabIndex = 10;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(374, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 15);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Fiyat";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(199, 48);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(87, 15);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Ürünün Miktarı";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(47, 45);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(68, 15);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "Ürünün Adı";
-            // 
             // label8
             // 
             this.label8.AutoSize = true;
@@ -248,6 +114,90 @@ namespace CafeAndRestaurant.Forms
             this.label8.TabIndex = 0;
             this.label8.Text = "Günlük Rapor                                                                     " +
     "                                                          ";
+            // 
+            // dgwAylikRapor
+            // 
+            this.dgwAylikRapor.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwAylikRapor.BackgroundColor = System.Drawing.Color.White;
+            this.dgwAylikRapor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgwAylikRapor.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgwAylikRapor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwAylikRapor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.urnAdiAylik,
+            this.urnMiktarAylik,
+            this.urnFiyatAylik});
+            this.dgwAylikRapor.GridColor = System.Drawing.Color.White;
+            this.dgwAylikRapor.Location = new System.Drawing.Point(21, 48);
+            this.dgwAylikRapor.Name = "dgwAylikRapor";
+            this.dgwAylikRapor.RowHeadersVisible = false;
+            this.dgwAylikRapor.RowTemplate.Height = 25;
+            this.dgwAylikRapor.Size = new System.Drawing.Size(422, 226);
+            this.dgwAylikRapor.TabIndex = 16;
+            // 
+            // urnAdiAylik
+            // 
+            this.urnAdiAylik.FillWeight = 150F;
+            this.urnAdiAylik.HeaderText = "Ürün Adı";
+            this.urnAdiAylik.Name = "urnAdiAylik";
+            // 
+            // urnMiktarAylik
+            // 
+            this.urnMiktarAylik.HeaderText = "Ürün Miktarı";
+            this.urnMiktarAylik.Name = "urnMiktarAylik";
+            // 
+            // urnFiyatAylik
+            // 
+            this.urnFiyatAylik.HeaderText = "Fiyat";
+            this.urnFiyatAylik.Name = "urnFiyatAylik";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.urunAdiGunluk,
+            this.urunMiktariGunluk,
+            this.urnFiyatGunluk});
+            this.dataGridView1.GridColor = System.Drawing.Color.White;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(422, 226);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // urunAdiGunluk
+            // 
+            this.urunAdiGunluk.FillWeight = 150F;
+            this.urunAdiGunluk.HeaderText = "Ürün Adı";
+            this.urunAdiGunluk.Name = "urunAdiGunluk";
+            // 
+            // urunMiktariGunluk
+            // 
+            this.urunMiktariGunluk.HeaderText = "Ürün Miktarı";
+            this.urunMiktariGunluk.Name = "urunMiktariGunluk";
+            // 
+            // urnFiyatGunluk
+            // 
+            this.urnFiyatGunluk.HeaderText = "Fiyat";
+            this.urnFiyatGunluk.Name = "urnFiyatGunluk";
             // 
             // Raporlar
             // 
@@ -263,6 +213,8 @@ namespace CafeAndRestaurant.Forms
             this.grpBoxGunlukRapor.PerformLayout();
             this.grpBoxAylikRapor.ResumeLayout(false);
             this.grpBoxAylikRapor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwAylikRapor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -270,22 +222,18 @@ namespace CafeAndRestaurant.Forms
         #endregion
 
         private GroupBox grpBoxGunlukRapor;
-        private ListBox lstBoxFiyatGunluk;
-        private ListBox lstBoxUrununMiktariGunluk;
-        private ListBox lstBoxUrununAdiGunluk;
         private Label label1;
-        private Label label5;
-        private Label label4;
-        private Label label3;
         private GroupBox grpBoxAylikRapor;
-        private ListBox lstBoxFiyatAylik;
-        private ListBox lstBoxUrununMiktariAylik;
-        private ListBox lstBoxUrununAdiAylik;
-        private Label label2;
-        private Label label6;
-        private Label label7;
         private Label label8;
         private Button button1;
         private Button button2;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn urunAdiGunluk;
+        private DataGridViewTextBoxColumn urunMiktariGunluk;
+        private DataGridViewTextBoxColumn urnFiyatGunluk;
+        private DataGridView dgwAylikRapor;
+        private DataGridViewTextBoxColumn urnAdiAylik;
+        private DataGridViewTextBoxColumn urnMiktarAylik;
+        private DataGridViewTextBoxColumn urnFiyatAylik;
     }
 }
